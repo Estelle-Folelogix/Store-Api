a667e6faed72237a476d012a52eac965727e23e0
Estelle-Folelogix <estelle.kouebou@folelogix.com>
2026-03-30 13:45:01 +0100

    feature: register

--- src/main/java/com/estelle/store/product/domain/service/UserService.java
+++ src/main/java/com/estelle/store/product/domain/service/UserService.java
@@ -1,4 +1,17 @@
 package com.estelle.store.product.domain.service;
 
+import com.estelle.store.product.domain.model.Users;
+import com.estelle.store.product.domain.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    public Users register(Users user){
+        return repo.save(user);
+    }
 }
